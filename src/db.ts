118a4d2483a25d8f29ea684396d7b8ef310005db
Abhijeet Arjun Aghade <abhijeet777@Abhijeets-MacBook-Air.local>
2025-12-25 15:43:33 +0530

    Updated Backend code

---
+++ src/db.ts
@@ -0,0 +1,22 @@
+import mongoose, { model, Schema } from "mongoose";
+mongoose.connect("mongodb+srv://abhi:Abhi1234@cluster0.wdkj6pd.mongodb.net/Brainly");
+
+const UserSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+
+})
+
+export const UserModel = model( "User", UserSchema);
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    type: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true },
+})
+export const ContentModel = model("Content", ContentSchema);
+
+ 
+ 
